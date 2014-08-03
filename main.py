21efbb519760fbf9aa221fd1bf65743cf6addf08
Andrea Belvedere <scieck@gmail.com>
2014-08-04 06:18:00 +0900

    add AddLocationForm to main.py file

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 class AddLocationForm(BoxLayout):
-    pass
+    def search_location(self):
+        print("Explicit is better than implicit.")
 
 class WeatherApp(App):
     pass
