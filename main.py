ece827cd390873c69b331482cc41cc7dbdc2dd97
Andrea Belvedere <scieck@gmail.com>
2014-08-04 20:03:16 +0900

    set search input id and property value

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
 
 class AddLocationForm(BoxLayout):
+    search_input = ObjectProperty()
+
     def search_location(self):
         print("Explicit is better than implicit.")
 
