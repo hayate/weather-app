7bd52dacdc32b44fd155003f5512de58e2a1b8d6
Andrea Belvedere <scieck@gmail.com>
2014-08-04 20:27:35 +0900

    retrieve map data and print list of cities

--- main.py
+++ main.py
@@ -4,12 +4,21 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
+from kivy.network.urlrequest import UrlRequest
+
 
 class AddLocationForm(BoxLayout):
     search_input = ObjectProperty()
 
     def search_location(self):
-        print("The user searched for '{0}'".format(self.search_input.text))
+        search_tpl = "http://api.openweathermap.org/data/2.5/find?q={0}&type=like"
+        search_url = search_tpl.format(self.search_input.text)
+        request = UrlRequest(search_url, self.found_location)
+
+    def found_location(self, request, data):
+        cities = ["{0}({1})".format(d['name'], d['sys'], ['country']) for d in data['list']]
+        print("\n".join(cities))
+
 
 class WeatherApp(App):
     pass
